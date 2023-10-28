4c96ea41a456404ffbce429b0a4390506901a0b5
Vitor Natario <natariovitor@gmail.com>
2023-10-28 13:58:50 -0300

    feat: create movie model

--- src/Movie.java
+++ src/Movie.java
@@ -1,4 +1,13 @@
+// lendo uma classe:
+// coisas que um filme tem
 public class Movie {
     String name;
+    int releaseYear;
+    boolean includedInThePlan;
+    double  filmReview;
+    int totalRatings;
+    int durationInMinutes;
+
+
 
 }
