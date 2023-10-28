df15706e2ba151185b267f93a955d368b3f4d069
Vitor Natario <natariovitor@gmail.com>
2023-10-28 14:23:31 -0300

    feat: add attributes to movie

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.name = "Elementos";
+        movie.releaseYear = 2023;
+        movie.durationInMinutes = 102;
+
+        System.out.println(movie.name);
+        System.out.println(movie.releaseYear);
+
+    }
+}
